a01d1b82c3f3168add13f7f4b6cfa06597c62b7c
l-Il <0rky@pm.me>
2021-03-16 15:40:09 +0300

    + russian language

--- mirea_title.py
+++ mirea_title.py
@@ -5,53 +5,64 @@ import os
 from pylatex import Command, Document, Figure, Foot, LargeText, LineBreak, MediumText, MiniPage, TextBlock, Package, PageStyle
 from pylatex.utils import bold
 
+
+class Title():
+    def __init__(self):
+        pass
+
+
 if __name__ == '__main__':
 
     # Настройки отступов, языка
     geometry_options = {"tmargin": "2cm", "lmargin": "3cm", "rmargin": "1.5cm", "bmargin": "2cm"}
     doc = Document(geometry_options=geometry_options)
     doc.packages.add(Package('grffile', options=['encoding', 'filenameencoding=utf8']))
+    doc.packages.add(Package('babel', options=['russian']))
 
     # Вставляем логотип МИРЭА
     image_filename = os.path.join(os.path.dirname(__file__), 'логотип.png')
     with doc.create(Figure(position='h!')) as logo:
         logo.add_image(image_filename, width='80px')
 
-    # Нижний колонтитул
-    header = PageStyle("header")
-    with header.create(Foot("C")):
-        header.append(f'Moscow, {time.now().timetuple().tm_year}\n')
-    doc.preamble.append(header)
-    doc.change_document_style("header")
-
     # Обложка
     doc.change_length("\TPHorizModule", "1mm")
     doc.change_length("\TPVertModule", "1mm")
     with doc.create(MiniPage(width=r"\textwidth")) as page:
 
+        # Центральная часть обложки
         with page.create(TextBlock(180, 0, 0)):
             page.append(Command('centering'))
-            page.append('MINOBRNAUKI ROSSII\n')
-            page.append('Federalnoe gosudarstvennoe budgetnoe obrazovatelnoe ucherezdenie vishego obrazovania\n')
-            page.append(MediumText(bold('<<MIREA - Rossiyski Texnologicheskiy Universitet\n')))
-            page.append(LargeText(bold('RTU MIREA\n')))
+            page.append('МИНОБРНАУКИ РОССИИ\n')
+            page.append('Федеральное государственное бюджетное образовательное учреждение высшего образования\n')
+            page.append(MediumText(bold('«МИРЭА - Российский Технологический Университет»\n')))
+            page.append(LargeText(bold('РТУ МИРЭА\n')))
             page.append(LineBreak())
-            page.append(MediumText(bold('Institut complex bezopasnosti i special priborostroyenia\n')))
+            page.append(MediumText(bold('Институт комплексной безопасности и специального приборостроения\n')))
             page.append(LineBreak())
-            page.append('Kafedra KB-8 <<Information Protivoborstvo>>\n')
+            page.append('Кафедра КБ-8 «Информационное Противоборство»\n')
             page.append(LineBreak())
             page.append(LineBreak())
             page.append(LineBreak())
-            page.append(LargeText('Otchet\n'))
-            page.append(f'po: {input("Введите название отчёта: > ")}\n')
+            page.append(LargeText('Отчёт\n'))
+            page.append(f'По: {input("Введите название отчёта: > ")}\n')
 
-        with page.create(TextBlock(80, 120, 120)):
-            page.append('Vipolnil:\n')
-            page.append(f'Student {input("Введите номер курса: > ")} kursa\n')
-            page.append(f'Gruppa {input("Введите свою группу: > ")}\n')
-            page.append(f'Shifr {input("Введите cвой шифр: > ")}\n')
-            page.append(f'Student {input("Введите свои ФИО: > ")}\n')
-            page.append('Proverila:\n')
+        # Правая часть обложки
+        with page.create(TextBlock(80, 88, 120)):
+            page.append('Выполнил:\n')
+            page.append(f'Студент {input("Введите номер курса: > ")} курса\n')
+            page.append(f'Группа {input("Введите свою группу: > ")}\n')
+            page.append(f'Шифр {input("Введите свой шифр: > ")}\n')
+            page.append(f'{input("Введите свои ФИО: > ")}\n')
+            page.append(LineBreak())
+            page.append(LineBreak())
+            page.append('Проверил:\n')
             page.append(f'{input("Введите ФИО преподавателя: > ")}\n')
+
+    # Нижний колонтитул
+    header = PageStyle("header")
+    with header.create(Foot("C")):
+        header.append(f'Москва, {time.now().timetuple().tm_year}\n')
+    doc.preamble.append(header)
+    doc.change_document_style("header")
 
     doc.generate_pdf("title", clean_tex=False)
